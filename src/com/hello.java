8bde161614ffed3afd1a1e599f12b9af9fe0963c
wsx1999 <71063973+wsx1999@users.noreply.github.com>
2020-09-11 11:57:19 +0800

    Update hello.java

--- src/com/hello.java
+++ src/com/hello.java
@@ -2,6 +2,6 @@ package com;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello Word!!!");
+        System.out.println("Hello Word!!!");!!!!!!
     }
 }
