6d91aace9e1dd3b972f18c676106900b79cee1b2
wsx <2590663104@qq.com>
2020-09-11 12:02:25 +0800

    初始化提交

--- src/com/hello.java
+++ src/com/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Word!!!");
         System.out.println("Hello Word!!!");
+        System.out.println("Hello Word!!!");
+        System.out.println("Hello Word!!!");
     }
 }
